21fe187362cd3d55ab7742cb673951a2d99b54f0
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-12-15 15:45:34 +0800

    refactor: 按照CUTI的栏结构生成json，并删掉无用代码
    同时把目前的engine.json内数据改key了

--- src/main.py
+++ src/main.py
@@ -9,109 +9,18 @@ syntax_3 = "<!-- MARKDOWN_TABLE END -->"
 def x_sort(data):
     def compare(dict_a: dict, dict_b: dict):
         dict_a_packagename = (
-            dict_a["package_name"].replace("_", "").replace("-", "").lower()
+            dict_a["engine_name"].replace("_", "").replace("-", "").lower()
         )
         dict_b_packagename = (
-            dict_b["package_name"].replace("_", "").replace("-", "").lower()
+            dict_b["engine_name"].replace("_", "").replace("-", "").lower()
         )
-        dict_a_ctan = dict_a["ctan_package"]
-        dict_b_ctan = dict_b["ctan_package"]
-        if dict_a_ctan == "" and dict_b_ctan == "":
-            if dict_a_packagename < dict_b_packagename:
+        if dict_a_packagename < dict_b_packagename:
                 return -1
-            if dict_a_packagename > dict_b_packagename:
-                return 1
-        elif dict_a_ctan == "" and dict_b_ctan != "":
-            return 1
-        elif dict_b_ctan == "" and dict_a_ctan != "":
-            return -1
-        else:
-            if dict_a_packagename < dict_b_packagename:
-                return -1
-            if dict_a_packagename > dict_b_packagename:
+        if dict_a_packagename > dict_b_packagename:
                 return 1
 
     data = sorted(data, key=cmp_to_key(compare))
     return data
-
-
-def badge(link: str, maintainer_type="组织", badge_type="github") -> str:
-    host = ["github", "gitlab", "gitee", "gitea"]
-    if link == "":
-        return link
-    if badge_type in host:
-        link_template = "https://img.shields.io/badge/{{user_name}}%2F{{repo_name}}-{{color}}?logo={{badge_type}}&link=https%3A%2F%2Fgithub.com%2F{{user_name}}%2F{{repo_name}}"
-        user_name = link.split("/")[1].replace("-", "--")
-        repo_name = link.split("/")[2].replace("-", "--")
-        if maintainer_type == "组织" or maintainer_type == "orgs":
-            link_template = link_template.replace("{{color}}", "blue")
-        elif maintainer_type == "个人" or maintainer_type == "user":
-            link_template = link_template.replace("{{color}}", "green")
-        else:
-            link_template = link_template.replace("{{color}}", "red")
-        link_template = (
-            link_template.replace("{{user_name}}", user_name)
-            .replace("{{repo_name}}", repo_name)
-            .replace("{{badge_type}}", badge_type)
-        )
-        return "![](" + link_template + ")"
-    elif badge_type == "ctan":
-        link_template = "https://img.shields.io/badge/ctan-{{thesis}}-lightgray?link=https%3A%2F%2Fctan.org%2Fpkg%2F{{thesis}}"
-        thesis_name = link.replace("ctan.org/pkg/", "")
-        link_template = link_template.replace("{{thesis}}", thesis_name)
-        return "![](" + link_template + ")"
-    else:
-        return link
-
-
-def repos(
-    maintainer_type: str,
-    repository_github: str,
-    repository_gitlab: str,
-    repository_gitee: str,
-    repository_gitea: str,
-):
-    repo_cell_template = "<div>{{repo_list}}</div>"
-    repo_list = []
-    if repository_github != "":
-        repo_list.append(
-            "<b>GitHub:</b><br/>"
-            + badge(
-                link=repository_github,
-                maintainer_type=maintainer_type,
-                badge_type="github",
-            )
-        )
-    if repository_gitlab != "":
-        repo_list.append(
-            "<b>GitLab Official:</b><br/>"
-            + badge(
-                link=repository_gitlab,
-                maintainer_type=maintainer_type,
-                badge_type="gitlab",
-            )
-        )
-    if repository_gitee != "":
-        repo_list.append(
-            "<b>Gitee:</b><br/>"
-            + badge(
-                link=repository_gitee,
-                maintainer_type=maintainer_type,
-                badge_type="gitee",
-            )
-        )
-    if repository_gitea != "":
-        repo_list.append(
-            "<b>Gitea Official:</b><br/>"
-            + badge(
-                link=repository_gitea,
-                maintainer_type=maintainer_type,
-                badge_type="gitea",
-            )
-        )
-
-    return repo_cell_template.replace("{{repo_list}}", "<br/>".join(repo_list))
-
 
 def markdown_row(data: list):
     string = ""
@@ -164,8 +73,8 @@ def markdown_entry(thesis_entry: dict):
 
 
 def markdown_gen(locale: str):
-    thesis_json = open("..\\data\\thesis.json", "r", encoding="utf-8")
-    thesis_data = json.loads(thesis_json.read())["CUTI"]
+    engine_json = open("..\\data\\engine.json", "r", encoding="utf-8")
+    engine_data = json.loads(engine_json.read())["engine_data"]
     column_json = open("..\\data\\column.json", "r", encoding="utf-8")
     column_data = json.loads(column_json.read())
     string = ""
@@ -176,10 +85,10 @@ def markdown_gen(locale: str):
     string += markdown_table(
         column_data["len"],
     )
-    # WRONG CODE: thesis_data.sort(key=lambda x: x["package_name"])
-    thesis_data = x_sort(thesis_data)
-    for i in range(len(thesis_data)):
-        string += markdown_entry(thesis_data[i])
+    # ALT CODE: engine_data.sort(key=lambda x: x["engine_name"])
+    engine_data = x_sort(engine_data)
+    for i in range(len(engine_data)):
+        string += markdown_entry(engine_data[i])
     return string
 
 
